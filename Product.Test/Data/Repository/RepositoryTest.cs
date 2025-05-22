e2a29e6e3a93bf6ce331794c024f45be1f78e30a
datpthe163028 <133310687+datpthe163028@users.noreply.github.com>
2025-05-22 22:23:36 +0700

    test: write repository test

---
+++ Product.Test/Data/Repository/RepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.Models;
+using Product.Test.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Test.Data.Repository
+{
+    public class RepositoryTest
+    {
+        private readonly CourseContext courseContext;
+        protected readonly DbSet<User> _dbSet;
+
+        public RepositoryTest()
+        {
+            courseContext = ContextFactory.Create();
+            _dbSet = courseContext.Set<User>();
+        }
+    }
+}
---
+++ Product.Test/Entity/CourseContextTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.Models;
+using Product.Test.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Test.Entity
+{
+    public class CourseContextTest
+    {
+        [Fact]
+        public void Constructure_CreateInMemoryDb_Success()
+        {
+            // Arrange
+            var context = ContextFactory.Create();
+            // Act
+            // Assert
+
+            Assert.True(context.Database.EnsureCreated());
+        }
+    }
+}
